975f3ee0c3f56bae15749e63e50815269c76d078
shamini667 <shaminis632@gmail.com>
2026-06-09 14:15:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector(".btn");
+button.onclick = function(){
+alert("Resume has downloaded");
+}
+
+let form = document.querySelector(".form");
+form.onsubmit = function(){
+alert("Message sent successfully!");
+}
